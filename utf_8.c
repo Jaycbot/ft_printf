b15fd59570dde04a80dc2ec55cabf2c7f16f0702
Jaehyeung Choi <jaehchoi@c1r7s2.42seoul.kr>
2021-01-18 01:52:09 +0900

    utf-8 done

--- utf_8.c
+++ utf_8.c
@@ -6,7 +6,7 @@
 /*   By: jaehchoi <jaehchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 22:25:50 by jaehchoi          #+#    #+#             */
-/*   Updated: 2021/01/18 00:49:19 by jaehchoi         ###   ########.fr       */
+/*   Updated: 2021/01/18 01:51:38 by jaehchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,39 @@
 static int  utf_8_4B(wint_t unicode)
 {
     unsigned char   unit;
-    
+
+    unit = (unicode >> 18 & 0x7) | 0xf0;
+    write(1, &unit, 1);
+    unit = (unicode >> 12 & 0x3f) | 0x80;
+    write(1, &unit, 1);
+    unit = (unicode >> 6 & 0x3f) | 0x80;
+    write(1, &unit, 1);
+    unit = (unicode & 0x3f) | 0x80;
+    write (1, &unit, 1);
     return (4);
 }
 
 static int  utf_8_3B(wint_t unicode)
 {
+    unsigned char   unit;
+
+    unit = (unicode >> 12 & 0xf) | 0xe0;
+    write(1, &unit, 1);
+    unit = (unicode >> 6 & 0x3f) | 0x80;
+    write(1, &unit, 1);
+    unit = (unicode & 0x3f) | 0x80;
+    write (1, &unit, 1);
     return (3);
 }
 
 static int  utf_8_2B(wint_t unicode)
 {
-    int ret;
+    unsigned char   unit;
 
+    unit = (unicode >> 6) | 0xc0;
+    write(1, &unit, 1);
+    unit = (unicode & 0x3f) | 0x80;
+    write(1, &unit, 1);
     return (2);
 }
 
@@ -35,20 +55,20 @@ static int utf_8_1B(wint_t unicode)
 {
     unsigned char   unit;
     
-    unit = 127 & unicode;
+    unit = 0x7f & unicode;
     write(1, &unit, 1);
     return (1);
 }
 
 int encode_utf_8(wint_t unicode)
 {
-    if (unicode | UTF_8_1B == UTF_8_1B)
+    if ((unicode | UTF_8_1B) == UTF_8_1B)
         return (utf_8_1B(unicode));
-    else if (unicode | UTF_8_2B == UTF_8_2B)
+    else if ((unicode | UTF_8_2B) == UTF_8_2B)
         return (utf_8_2B(unicode));
-    else if (unicode | UTF_8_3B == UTF_8_3B)
+    else if ((unicode | UTF_8_3B) == UTF_8_3B)
         return (utf_8_3B(unicode));
-    else if (unicode | UTF_8_4B == UTF_8_4B)
+    else if ((unicode | UTF_8_4B) == UTF_8_4B)
         return (utf_8_4B(unicode));
     return (-1);
 }
