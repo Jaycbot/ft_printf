ed0048e1b2f237220acd423e2a174db0ceed1fd5
Jaehyeung Choi <jaehchoi@c1r7s5.42seoul.kr>
2021-01-19 01:08:12 +0900

    percent added

--- perc_parse.c
+++ perc_parse.c
@@ -6,13 +6,42 @@
 /*   By: jaehchoi <jaehchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 22:02:49 by jaehchoi          #+#    #+#             */
-/*   Updated: 2021/01/18 22:04:18 by jaehchoi         ###   ########.fr       */
+/*   Updated: 2021/01/19 01:07:56 by jaehchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int    parse_perc(t_contents *contents)
+static int	parse_width(t_contents *f, int c)
 {
-    
+	int i;
+
+	f->width = (f->width < 0) ? -f->width : f->width;
+	i = f->width - 1;
+	if (f->minus)
+	{
+		ret_with_write(c);
+		while (i--)
+			ret_with_write(' ');
+	}
+	else if (f->zero)
+	{
+		while (i--)
+			ret_with_write('0');
+		ret_with_write(c);
+	}
+	else
+	{
+		while (i--)
+			ret_with_write(' ');
+		ret_with_write(c);
+	}
+	return ((1 < f->width) ? 1 : f->width);
+}
+
+int			parse_perc(t_contents *contents)
+{
+	if (contents->width < 0)
+		contents->minus = 1;
+	return (parse_width(contents, '%'));
 }
